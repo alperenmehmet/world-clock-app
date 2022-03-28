da30ab1294c726a04038e73b0ecce729fff9baac
Mehmet ALPEREN <me.alperen@gmail.com>
2022-03-28 22:11:13 +0300

    Quotes api call finished.

--- src/context.js
+++ src/context.js
@@ -1,9 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { BASE_URL_QUOTE } from './constants/api';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={'hello'}>{children}</AppContext.Provider>;
+  const [quote, setQuote] = useState([]);
+  const fetchQuotes = useCallback(async () => {
+    try {
+      const response = await fetch(`${BASE_URL_QUOTE}`);
+      const data = await response.json();
+      setQuote(data);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  useEffect(() => {
+    fetchQuotes();
+  }, [fetchQuotes]);
+  return (
+    <AppContext.Provider value={{ quote, setQuote }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
